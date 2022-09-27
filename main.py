6f6a5d8e97f3c1ed994101e91e1027b4e1024158
Ironmanblast8 <pickingemailsarehard@gmail.com>
2022-09-27 22:27:36 +1300

    New Card Game
    A complete refresh of the old game to better suit the programming scenario and the club's way of using it.

--- main.py
+++ main.py
@@ -1,146 +1,182 @@
-from logging import root
-import random
-import tkinter as tk
+import random # Random shuffles list (Deck of cards)
+import tkinter as tk # Importing tkinter for tkobjects
 from tkinter import *
+from tkinter import filedialog as fd # Lets users load files
 from tkinter import ttk
-from tkinter import filedialog as fd
-import tkinter
 from PIL import Image, ImageTk
-from os.path import exists
 
-bgcolour = ('firebrick1') #Sets bgcolour as global variable
+bgcolour = ('lightblue') # Sets bgcolour as global variable so that multiple classes/methods can use it. 
 
-class load():
-    def __init__(self):
-        try:
-            global cardDeck
-            #Creating tkinter window
-            self.root = tk.Tk()
-            self.root.title('Card Games')
-            self.root.geometry('500x250')
-            self.root.config(bg=bgcolour)
-            self.loadingLabel = tk.Label(self.root, text="Loading...", bg=bgcolour, font=('verdana', 40), fg='green') #f user is on a slow pc the loading label should say loading, So the user knows something is happening.
-            self.loadingLabel.place(x=100, y=80) #places label at these pixels
-
-            self.loadingLabel["text"] = "Choose game file."  #Changes the label to choose game file when the program is ready to load a game file
-            self.loadingLabel.place(x=10, y=80) #places label at these pixels
-            main_file = fd.askopenfile()
-            file = main_file.readline()
-            file = file[:-1]
-            deck = open(file, "r")
-            n = 2
-            card_deck = deck.readline()
-            cardDeck = ([card_deck[i:i+n] for i in range(0, len(card_deck), n)])
-            random.shuffle((cardDeck))
-            # open file in write mode
-            with open(r'carddeck.txt', 'w') as fp:
-                for item in cardDeck:
-                    # write each item on a new line
-                    fp.write("%s\n" % item)
-                print('Done')
-            self.cont()
-            self.root.mainloop()
-        except:
-            print("Unexpected error")
-
-    def cont(self):
-        self.loadingLabel["text"] = "Continuing"  #If user is on a slow pc the loading label should change to continuing, So the user knows something is happening.
-        self.loadingLabel.place(x=90, y=70) #places label at these pixels)
-        self.root.destroy() #Destroys the small tkinter window
-        gameSelector() #Calls the game selector function
-
-
-def gameSelector():
-    global cardDeck
-    #Creating tkinter window
+# Settings for the card game
+def settings():
+    # Creating tkinter window
     root = Tk()
+    root.title('Card Game - Settings')
+    root.geometry('960x1080')
+    root.config(bg=bgcolour)
+
+    def settingsLoad():
+        try:
+            # Grabs the entry of the 
+            hands = int(entry.get())
+            numCards = int(entryNum.get() )
+            numDecks = int(entryDeck.get())
+            # If any of the boxes aren't greater then 0 It will forcibly cause a value error to occur.
+            if 0 >= hands:
+                int("two")
+            if 0 >= numCards:
+                int("two")
+            if 0 >= numDecks:
+                int("two")
+            
+            # If no errors in the settings load closes the menu and calls the play method
+            root.destroy()
+            play(hands, numCards, numDecks)
+        except ValueError:
+            errorScreen= Toplevel(root)
+            errorScreen.geometry("1235x200")
+            errorScreen.title("Error Window")
+            errorScreen.config(bg='red')
+            Label(errorScreen, text= "Please enter integer value into settings", bg='red', font=('Courier 38 bold')).grid(column=0, row=1, sticky=EW)
+            Label(errorScreen, text= "Might need to also be a positive integer", bg='red', font=('Courier 15 bold')).grid(column=0, row=2, sticky=S)
+        
+
+    # Initialize a Label
+    label=Label(root, text="Hands to play:", bg=bgcolour, font=("Courier 22 bold"))
+    label.pack()
+
+    # Create an Entry widget to accept User Input
+    entry= Entry(root, width= 40)
+    entry.focus_set()
+    entry.pack()
+
+    # Initialize a Label
+    label=Label(root, text="Amount of Cards in a hand:", bg=bgcolour, font=("Courier 22 bold"))
+    label.pack()
+
+    # Create an Entry widget to accept User Input
+    entryNum= Entry(root, width= 40)
+    entryNum.focus_set()
+    entryNum.pack()
+
+    # Initialize a Label
+    label=Label(root, text="Number of decks:", bg=bgcolour, font=("Courier 22 bold"))
+    label.pack()
+
+    # Create an Entry widget to accept User Input
+    entryDeck= Entry(root, width= 40)
+    entryDeck.focus_set()
+    entryDeck.pack()
+
+    # Create a Button to validate Entry Widget, Then calls the settings load method to make sure teh settings are correct.
+    button = ttk.Button(root, text= "Enter Settings",width= 100, command= settingsLoad)
+    button.pack()
+
+    # Loops the windows so it doesn't close on it
+    root.mainloop()
+
+
+
+# The play method is passed the values from the settings method
+def play(hands, numCards, numDecks):
+    print(hands)
+    print(numCards)
+    print(numDecks)
+    
+    # Creating tkinter window with a screen of 500x250
+    root = tk.Tk()
     root.title('Card Games')
+    root.geometry('500x250')
+    root.config(bg=bgcolour)
+
+    loadingLabel = tk.Label(root, text="Loading...", bg=bgcolour, font=('verdana', 40), fg='black') # If user is on a slow pc the loading label should say loading, So the user knows something is happening.
+    loadingLabel.place(x=100, y=80) # Places label at these pixels
+    loadingLabel["text"] = "Choose game file."  # Changes the label to choose game file when the program is ready to load a game file
+    loadingLabel.place(x=10, y=80) # Places label at these pixels
+
+    main_file = fd.askopenfile()
+    file = main_file.readline() #Reads the first line in the file to then load in the text file of the deck hand. 
+    file = file[:-1]
+    # numberCards = main_file.readline()
+    deck = open(file, "r")
+    n = 2
+    card_deck = deck.readline()
+    cardDeck = [card_deck[i:i+n] for i in range(0, len(card_deck), n)] * numDecks
+    print(cardDeck)
+    random.shuffle(cardDeck) # Shuffles the cardDeck list. 
+    # Compresses the carDeck list so you can split it up into hands.
+    cardDeckStr = ' '.join([str(elem) for elem in cardDeck])
+    cardDeckStr = cardDeckStr.replace(" ", "")
+    cardHandDecks = [cardDeckStr[i*numCards*n:(i*numCards*n)+numCards*n] for i in range(hands)]
+    print(cardDeck)
+    print(cardDeckStr)
+    print(cardHandDecks)
+    root.destroy()
+
+    # Creating tkinter window of the game, We have to re create it as we destroyed the previous one.
+    root = tk.Tk()
+    root.title('Card Game')
     root.geometry('1920x1080')
     root.config(bg=bgcolour)
 
-    # Declaring constrants
-    padx = 10
-    pady = 10
-    card = 0
-    buttonRow = 0
-    menuRow = 0
-    menuCol = 0
-    cards = []
-    gridSize = 10
-
-    #Sets up the cardGames to play
-    cardGames = [' Memory',' Hearts',' Last Card',' Go Fish',' More to come']
-
-    #labels
-    tk.Label(root, text ="Card Games", fg='DarkGreen', bg=bgcolour, font = ("Times New Roman", 50)).grid(column = 2, row = 0, padx = padx, pady = pady)
-    tk.Label(root, text = "Card Game :", bg=bgcolour, font = ("Times New Roman", 50)).grid(column = 0, row = 1, padx = padx, pady = pady)
-
-    #Combobox creation
-    n = tk.StringVar()
-    gameChoosen = ttk.Combobox(root, textvariable = n)
-    # Adding combobox drop down list using the cardGames list 
-    gameChoosen['values'] = (cardGames)  
-    gameChoosen.grid(column = 1, row = 1)
-    #gameChoosen.current()
-
-    def gameChanged():
-        errorWindow = tk.Tk()
-        errorWindow.title("Error")
-        #print(gameChoosen.get())
-        if gameChoosen.get() in cardGames:
-            cardGame = gameChoosen.get() #Makes cardgame the gamechoosen
-            errorWindow.destroy()
-            launchGame(cardGame)
-        elif gameChoosen.get() == "":
-            label = ttk.Label(errorWindow, text="Blank Card Game, Enter value")
-            label.pack(side="top", fill="x", pady=10)
-            errorLoad = tk.Button(errorWindow, text="Okay", command = errorWindow.destroy)
-            errorLoad.pack()
-        else:
-            label = ttk.Label(errorWindow, text="Invalid card game try again, Might not exist yet")
-            label.pack(side="top", fill="x", pady=10)
-            errorLoad = tk.Button(errorWindow, text="Okay", command = errorWindow.destroy)
-            errorLoad.pack()
-        
-        errorWindow.mainloop()
-
-    gameSelectButton = tk.Button(root, text="Select Game", font=(200), compound="center", borderwidth=0, bg='red', highlightthickness=0, command= gameChoosen.bind('<<ComboboxSelected>>', gameChanged))
-    gameSelectButton.grid(column = 1, row = 2, padx = padx, pady = pady)
-    cardsFrame = tk.Frame(root)
+    # Variables
+    cardsFrame = tk.Frame(root, bg=bgcolour)
     cardsFrame.grid(rows=1, column=2)
-    #print(cardDeck)
-    while card < len(cardDeck):
-        cardImg = (Image.open(f"cards/{cardDeck[card]}.gif"))
-        cardImg = cardImg.resize((108 ,151)) # Halfs the size of the large playing cards. For the loading screen this is cause we don't need it to be large
-        cardImg = ImageTk.PhotoImage(cardImg)
-        cards.append(cardImg)
-        card += 1
-        button=tk.Button(cardsFrame, image=cardImg, command=lambda:  print(cardImg))
-        if buttonRow < gridSize:
-            button.grid(row=menuRow,column=menuCol)
-            buttonRow += 1
-            menuCol += 1
-        if buttonRow == gridSize:
-             menuRow += 1        
-             buttonRow = 0
-             menuCol = 0
- 
-    root.mainloop()
-    
-def launchGame(cardGame):
-    loadGame = cardGame.replace(" ", "").lower()
-    if exists(loadGame+".py"):
-        exec(open(loadGame+".py").read())
-        root.destroy()
-    else:
-        errorWindow = tk.Tk()
-        label = ttk.Label(errorWindow, text="Card game doesn't exist yet, Maybe add it?")
-        label.pack(side="top", fill="x", pady=10)
-        errorLoad = tk.Button(errorWindow, text="Okay", command = errorWindow.destroy)
-        errorLoad.pack()
-        errorWindow.mainloop()
+    gridSize = 10 # Sets the grid size so cards only go 10 cards across.
+    handsDealt = 0
+    handRow = 0
 
-#If the name of the program is called main it will run everything below.
+    # Will display the hand of cards
+    def sel(i): # Changes the label to tell user what hand is showing.
+        buttonRow = 0
+        menuRow = 0
+        menuCol = 0
+        player = i-1
+        card = 0
+        handLabel.set("You selected hand number " + str(i))
+        
+        print(cardDeckStr)
+        while card < numCards:
+            try:
+                print("dfs")
+                print(player)
+                localCardHandDecks = [cardHandDecks[player][x:x+n] for x in range(0, len(cardHandDecks[player]), n)]
+                print(localCardHandDecks)
+            except IndexError:
+                print(IndexError)
+                pass
+            cardImg = (Image.open(f"cards/{localCardHandDecks[card]}.gif"))
+            cardImg = cardImg.resize((108 ,151)) # Half's the size of the large playing cards. For the loading screen this is cause we don't need it to be large3
+            cardImg = ImageTk.PhotoImage(cardImg)
+            cardImg.im = cardImg
+            card += 1
+            button=tk.Button(cardsFrame, image=cardImg, command=lambda i=card: print(i), bg=bgcolour)
+            if buttonRow < gridSize:
+                button.grid(row=menuRow,column=menuCol)
+                buttonRow += 1
+                menuCol += 1
+            if buttonRow == gridSize:
+                    menuRow += 1        
+                    buttonRow = 0
+                    menuCol = 0 # Resets menuCol to its default of 0
+
+
+    while handsDealt < hands:
+        handsDealt += 1 # Adds one to handsDealt
+        handLabel = StringVar()
+        # Creates a label widget
+        tk.Label(root, textvariable = handLabel).grid(row=0, column=11)
+        handLabel.set("What hand to display?")
+        handsButton = Radiobutton(root, text="Show hand " + str(handsDealt), value= handsDealt, command=lambda i=handsDealt: sel(i))
+        handsButton.grid(row=1+handRow, column=11)
+        handRow += 1
+
+    root.mainloop()
+
+
+    
+
+
+# If the name of the program is called main it will run everything below.
 if __name__ == '__main__':
-    #gameSelector()
-    load()
+    settings() # Runs the settings method
